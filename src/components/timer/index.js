2127d858f0c440a4d2bc6d91f7a2863e51494578
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-25 13:20:48 -0800

    working on timer

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import "./index.css";
 
 export default class Timer extends Component {
-
+  state = {
+    time: 0,
+  };
+  componentDidMount() {
+    this.timer(this.props.initial);
+    window.location = "/";
+  }
+  timer = (time) => {
+    for (let i = time; i > -1; i--) {
+      this.setState({ time: i });
+    }
+  };
   render() {
+    let { time } = this.state;
     return (
       <div className="mt-100 layout-column align-items-center justify-content-center">
-         <div className="timer-value" data-testid="timer-value"></div>
-         <button className="large" data-testid="stop-button">Stop Timer</button>
+        <div className="timer-value" data-testid="timer-value">
+          {time}
+        </div>
+        <button className="large" data-testid="stop-button">
+          Stop Timer
+        </button>
       </div>
     );
   }
 }
-
